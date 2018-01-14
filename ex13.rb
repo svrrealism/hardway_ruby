e409e682d4e8d6e58358717df716f8c99a54b920
svrrealism <svrrealists@gmail.com>
2018-01-14 15:07:41 -0500

    Add exercise 13 with study drill script, 13a.

---
+++ ex13.rb
@@ -0,0 +1,11 @@
+# Exercise 13: Parameters, Unpacking, Variables
+
+
+first, second, third = ARGV
+print "Please enter a fourth item: "
+fourth = $stdin.gets.chomp
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "Your fourth variable is: #{fourth}"
---
+++ ex13a.rb
@@ -0,0 +1,9 @@
+favorite_artist, favorite_artist2 = ARGV
+
+puts "Your favorite artists are #{favorite_artist} and #{favorite_artist2}."
+
+
+print "What is your favorite animal?: "
+favorite_animal = $stdin.gets.chomp
+
+puts "#{favorite_animal}"
