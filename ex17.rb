cbe1a13cc8367909b2fa8091eb7f2e0b9db85a25
svrrealism <svrrealists@gmail.com>
2018-01-14 17:50:12 -0500

    Made ex17 file more seamless to use.

--- ex17.rb
+++ ex17.rb
@@ -12,34 +12,34 @@ puts "Copying from #{from_file} to #{to_file}"
 indata = open(from_file).read
 
 #Chars take up one byte so the length of the chars is the number of bytes
-puts "The input file is #{indata.length} bytes long"
-
-
-spaces = "\n"*5
-lines = "=="*10
-
-puts """
-
-So far, the classes that exist in this script are the following:
-#{lines}
-#{spaces}
-
-From File or ARGV[0]: #{from_file.class}
-To File or ARGV[1]: #{to_file.class}
-In Data: #{indata.class}
-open(from_file): #{open(from_file).class}
-open(from_file): #{open(from_file).object_id}
-open(from_file): #{open(from_file).close.class}
-
-#{spaces}
-#{lines}
-"""
-
-
-#See if you can remove the parenthetics from to_file
-puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
-$stdin.gets
+# puts "The input file is #{indata.length} bytes long"
+#
+#
+# spaces = "\n"*5
+# lines = "=="*10
+#
+# puts """
+#
+# So far, the classes that exist in this script are the following:
+# #{lines}
+# #{spaces}
+#
+# From File or ARGV[0]: #{from_file.class}
+# To File or ARGV[1]: #{to_file.class}
+# In Data: #{indata.class}
+# open(from_file): #{open(from_file).class}
+# open(from_file): #{open(from_file).object_id}
+# open(from_file): #{open(from_file).close.class}
+#
+# #{spaces}
+# #{lines}
+# """
+#
+#
+# #See if you can remove the parenthetics from to_file
+# puts "Does the output file exist? #{File.exist?to_file}"
+# puts "Ready, hit RETURN to continue, CTRL-C to abort."
+# $stdin.gets
 
 #This line truncates whatever was specified in ARGV[0]
 # out_file = open(to_file, 'w')
