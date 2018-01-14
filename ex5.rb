e32861956b3e594334d5373770764a6bcb0fe560
svrrealism <svrrealists@gmail.com>
2018-01-13 20:16:14 -0500

    Add exercise 5

---
+++ ex5.rb
@@ -0,0 +1,21 @@
+name = 'Svrrealist'
+age = 30 #not a lie in 2018
+height = 71 #inches
+centi_height = height * 2.54
+weight = 190 #lbs
+kilo_weight = weight / 2.2
+eyes = "brown"
+teeth = "white"
+hair = "brown"
+
+puts "Let's talk about #{name}"
+puts "They are #{height} inches tall"
+puts "They are #{weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "They have #{eyes} eyes and #{hair} hair."
+puts "Their teeth are usually #{teeth} depending on the covfefe."
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+puts "Height in centimeters is #{centi_height}."
+puts "Weight in kilograms is #{kilo_weight}."
