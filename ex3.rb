be38ac581158f3fa457af15aa237823bb8cca3f0
svrrealism <svrrealists@gmail.com>
2018-01-13 19:40:41 -0500

    Add exercise 3

---
+++ ex3.rb
@@ -0,0 +1,42 @@
+# outputs a string about chickens
+puts "I will now count my chickens:"
+
+# Outputs 30
+puts "Hens #{25 + 30 / 6}"
+
+#Outputs 97
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#Outputs another string
+puts "Now I will count the eggs:"
+
+#Outputs 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+
+#Outputs a string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Calculates the above question for an answer of false
+puts 3 + 2 < 5 - 7
+
+#Outputs 5
+puts "What is 3 + 2? #{3 + 2}"
+
+Outputs -2
+puts "What is 5 - 7? #{5 - 7}"
+
+#Outputs a string
+puts "Oh, that's why it's false."
+
+#Outputs another string
+puts "How about some more"
+
+#Outputs true
+puts "Is it greater? #{5 > -2}"
+
+#Outputs true
+puts "Is it greater or equal? #{5 >= -2}"
+
+#Outputs false
+puts "Is it less or equal? #{5 <= -2}"
