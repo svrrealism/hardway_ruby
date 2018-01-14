0b623703d808bdc70df745e927c716be0fc1e866
svrrealism <svrrealists@gmail.com>
2018-01-14 14:35:12 -0500

    Add exercise 11 with additional questions about job, pets, and food.

---
+++ ex11.rb
@@ -0,0 +1,16 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "What is your job occupation? "
+job = gets.chomp
+print "What is your favorite food? "
+food = gets.chomp
+print "How many pets do you have?"
+number_of_pets = gets.chomp
+
+
+puts "So you're #{age} old, #{height} tall and #{weight} heavy. 
+You work as a #{job}. Your favorite food is #{food} and you have #{number_of_pets} pets."
