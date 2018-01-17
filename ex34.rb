399fc66ada304035d11907fb1fa40a1bb3975dea
svrrealism <svrrealists@gmail.com>
2018-01-16 20:31:28 -0500

    Add exercise 34.

---
+++ ex34.rb
@@ -0,0 +1,36 @@
+#Exercise 34: Accessing Elements of Arrays
+
+animals = %w(
+  bear
+  ruby
+  peacock
+  kangaroo
+  whale
+  platypus
+)
+
+puts animals
+
+#1 The animal at 1
+puts animals[1]
+
+#2 The third(3rd) animal
+puts animals[2]
+
+#3 The first animal
+puts animals[0]
+
+#4 The animal at 3
+puts animals[3]
+
+#5 The fifth (5th) animal
+puts animals[4]
+
+#6 The animal at 2
+puts animals[2]
+
+#7 The sixth (6th) animal
+puts animals[5]
+
+#8 The animal at 4
+puts animals[4]
