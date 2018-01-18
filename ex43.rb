2e76b4758441ac19e88cc7c4dd8ce748bea7e0d2
svrrealism <svrrealists@gmail.com>
2018-01-18 18:40:06 -0500

    Add steps for object oriented design.

---
+++ ex43.rb
@@ -0,0 +1,14 @@
+#Exercise 43: Basic Object-Oriented Analysis and Design
+
+puts """
+When you want to build something in Ruby, specifically with OOP
+
+1. Write or Draw about the problem
+2. Extract key concepts from 1 and research them
+3. Create a class hierarchy and object map for the concepts
+4. Code the classes and a test to run them
+5. Repeat and refine
+
+
+
+"""
