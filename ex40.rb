851e5c557a4ee8ee70d1e344024c98370cf17101
svrrealism <svrrealists@gmail.com>
2018-01-17 22:32:20 -0500

    Complete ex 40, OOP.

---
+++ ex40.rb
@@ -0,0 +1,23 @@
+# Exercise 40: Modules, Classes, and Objects
+
+class Song
+
+  def initialize(lyrics)
+    @lyrics = lyrics
+  end
+
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line}
+  end
+end
+
+happy_bday = Song.new(["Happy Birthday to you",
+   "I don't want to get sued",
+   "So I'll stop right here"])
+
+bulls_on_parade = Song.new(["The rally around tha family",
+  "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
