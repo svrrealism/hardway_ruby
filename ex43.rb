65333b4e099fcaae4ea51cf428a3594bc5ca3eb8
svrrealism <svrrealists@gmail.com>
2018-01-18 20:16:12 -0500

    Make game talk at menu if kernel is macOS.

--- ex43.rb
+++ ex43.rb
@@ -1,23 +1,35 @@
 #Exercise 43: Basic Object-Oriented Analysis and Design
 
-puts """
-When you want to build something in Ruby, specifically with OOP, you have:
+puts "Game or Read Theory (g/r)?"
+print "> "
+answer = $stdin.gets.chomp.downcase!
 
-Top Down
---------
-1. Write or Draw about the problem
-2. Extract key concepts from 1 and research them
-3. Create a class hierarchy and object map for the concepts
-4. Code the classes and a test to run them
-5. Repeat and refine
+if answer == 'r'
 
-Bottom Up
----------
-1. Take a small piece of the problem, hack on some code and get it to run barely.
-2. Refine the code to something more formal with classes and automated tests.
-3. Extract the key concepts you're using and try to find research for them.
-4. Write a description of what's really going on.
-5. Go back and refine the code, possibly thowing it out and starting over.
-6. Repeat, mvoing on to some other piece of the problem.
+  puts """
+  When you want to build something in Ruby, specifically with OOP, you have:
 
-"""
+  Top Down
+  --------
+  1. Write or Draw about the problem
+  2. Extract key concepts from 1 and research them
+  3. Create a class hierarchy and object map for the concepts
+  4. Code the classes and a test to run them
+  5. Repeat and refine
+
+  Bottom Up
+  ---------
+  1. Take a small piece of the problem, hack on some code and get it to run barely.
+  2. Refine the code to something more formal with classes and automated tests.
+  3. Extract the key concepts you're using and try to find research for them.
+  4. Write a description of what's really going on.
+  5. Go back and refine the code, possibly thowing it out and starting over.
+  6. Repeat, mvoing on to some other piece of the problem.
+
+  """
+else
+  welcome = "Welcome to Gothons from Planet Percal #25"
+
+  puts welcome
+  puts `say -r 250 -v Bruce #{welcome}` if /darwin/ =~ `uname -a`.downcase
+end
