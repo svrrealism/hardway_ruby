5861a2ca5e8251b2e308a6db2d9f63cb6934b8d6
svrrealism <svrrealists@gmail.com>
2018-01-18 19:07:52 -0500

    Distinguishment between Top Down vs. Bottom Up development.

--- ex43.rb
+++ ex43.rb
@@ -1,14 +1,23 @@
 #Exercise 43: Basic Object-Oriented Analysis and Design
 
 puts """
-When you want to build something in Ruby, specifically with OOP
+When you want to build something in Ruby, specifically with OOP, you have:
 
+Top Down
+--------
 1. Write or Draw about the problem
 2. Extract key concepts from 1 and research them
 3. Create a class hierarchy and object map for the concepts
 4. Code the classes and a test to run them
 5. Repeat and refine
 
-
+Bottom Up
+---------
+1. Take a small piece of the problem, hack on some code and get it to run barely.
+2. Refine the code to something more formal with classes and automated tests.
+3. Extract the key concepts you're using and try to find research for them.
+4. Write a description of what's really going on.
+5. Go back and refine the code, possibly thowing it out and starting over.
+6. Repeat, mvoing on to some other piece of the problem.
 
 """
