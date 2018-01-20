f84efe7cd38be0189fc06afef6c0aaf3dd01ab40
svrrealism <svrrealists@gmail.com>
2018-01-20 14:23:49 -0500

    Explicit inheritance and super method.

--- ex44.rb
+++ ex44.rb
@@ -1,16 +1,56 @@
 #Exercise 44: Inheritance Versus Composition
 
-class Parent
-  def implicit
-    puts "PARENT implicit()"
-  end
-end
+# class Parent
+#   def implicit
+#     puts "PARENT implicit()"
+#   end
+# end
+#
+# class Child < Parent
+# end
+#
+# dad = Parent.new
+# son = Child.new
+#
+# dad.implicit
+# son.implicit
 
-class Child < Parent
-end
+# class Parent
+#   def override()
+#     puts "PARENT override()"
+#   end
+#
+# end
+#
+# class Child
+#   def override()
+#     puts "CHILD override()"
+#   end
+# end
+#
+# dad = Parent.new
+# son = Child.new
+#
+# puts dad.override()
+# puts son.override()
 
-dad = Parent.new
-son = Child.new
-
-dad.implicit
-son.implicit
+# class Parent
+#   def altered()
+#     puts "PARENT altered()"
+#   end
+# end
+#
+# class Child < Parent
+#   def altered()
+#     puts "CHILD, BEFORE PARENT altered()"
+#     super()
+#     puts "CHILD, AFTER PARENT altered()"
+#   end
+# end
+#
+# dad = Parent.new
+# son = Child.new
+#
+# dad.altered
+# puts '-' * 10
+# son.altered
