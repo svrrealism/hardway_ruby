3f6fb96e15be87a211e7cac4a2ce9ad24264f576
svrrealism <svrrealists@gmail.com>
2018-01-20 13:24:57 -0500

    Show implicit method called through parent and inherited parent class.

---
+++ ex44.rb
@@ -0,0 +1,16 @@
+#Exercise 44: Inheritance Versus Composition
+
+class Parent
+  def implicit
+    puts "PARENT implicit()"
+  end
+end
+
+class Child < Parent
+end
+
+dad = Parent.new
+son = Child.new
+
+dad.implicit
+son.implicit
